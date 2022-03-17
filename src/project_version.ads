2e89fccf8cfd7639e1b14a464fb0209100887dc7
saulius <saulius@bca78af1-2ad7-44b6-bc0e-2ec9bff93b0d>
2022-03-17 10:47:03 +0000

    crystal-ada/ (saulius@kolibris) Adding version generation and release management Makefiles. Testing the version generation Makefile.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/crystal-ada/trunk@95 bca78af1-2ad7-44b6-bc0e-2ec9bff93b0d

--- src/project_version.ads
+++ src/project_version.ads
@@ -1,5 +1,5 @@
 package Project_Version is
    
-   Version : constant String := "0.1.0";
+   Version : constant String := "0.1.0 WIP Revision: 278 URL: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/makefiles/versions-release-in-filesystem/.version ";
    
 end Project_Version;
