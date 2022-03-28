3242d852836732e36ca65ca4134c21cb2341d72e
saulius <saulius@bca78af1-2ad7-44b6-bc0e-2ec9bff93b0d>
2022-03-28 07:52:01 +0000

    trunk/ (saulius@kukutis) Adding two tests to see how Ada reacts to XYZ files with wrong numbers of atoms specified.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/crystal-ada/trunk@114 bca78af1-2ad7-44b6-bc0e-2ec9bff93b0d

---
+++ tests/cases/xyzformat_015.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+./bin/xyzformat \
+    tests/data/t1-just-right-number-of-atoms.xyz \
+    tests/data/t3-too-many-atoms.xyz \
+    tests/data/t1-just-right-number-of-atoms.xyz \
+    #
---
+++ tests/cases/xyzformat_016.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+./bin/xyzformat \
+    tests/data/t1-just-right-number-of-atoms.xyz \
+    tests/data/t2-too-few-atoms.xyz \
+    tests/data/t1-just-right-number-of-atoms.xyz \
+    #
