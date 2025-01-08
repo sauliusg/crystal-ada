204fc1762ec327bdafb8bf39c6a2930bd52cc48d
saulius <saulius@bca78af1-2ad7-44b6-bc0e-2ec9bff93b0d>
2025-01-08 13:12:12 +0000

    Creating branch for the 0.2.0 version release.
    crystal-ada/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/crystal-ada/trunk@134 bca78af1-2ad7-44b6-bc0e-2ec9bff93b0d

--- src/project_version.ads
+++ src/project_version.ads
@@ -1,5 +1,5 @@
 package Project_Version is
    
-   Version : constant String := "0.2.0 WIP Revision: 95 URL: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/crystal-ada/trunk/.version ";
+   Version : constant String := "0.2.1 WIP Revision: 294 URL: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/makefiles/versions-release-in-filesystem/Makeconfig-version-svn.example ";
    
 end Project_Version;
